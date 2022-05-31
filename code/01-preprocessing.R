6d277ef48c05ecb15baf3258647f3e1619757d2f
Shaun Tan <stan@35292brkdt.icr.ac.uk>
2022-05-31 12:20:31 +0100

    Added code for preprocessing on id response combination

--- code/01-preprocessing.R
+++ code/01-preprocessing.R
@@ -5,8 +5,10 @@ cna_annotated <- read.table(here::here("data/consensus_cnv/consensus.20170119.so
   # For one individual's data
 consensus_cn_gene <- read.table(here::here("data/consensus_cnv/gene_level_calls/all_samples.consensus_CN.by_gene.170214.txt"), fill = TRUE)
 platinum_response <- read.table(here::here("data/TCGA_OV_PlatinumResponse_PMID27526849.txt"), header = TRUE)
+id_mapping <- read.table(here::here("data/id_mapping_icgc2tcga.45_donors.txt"), header = TRUE)
 
 
 # Processing Steps --------------------------------------------------------
-
+## Merging platinum response and the IDs of ICGC and TCGA 45 donors together
+id_with_response <- merge(platinum_response, id_mapping, by.x = "SampleCode", by.y = "TCGA_id")
 
