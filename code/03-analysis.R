cef86a63d3bc695fb21a86b88c03d9330b5334a2
Shaun Tan <stan@35292brkdt.icr.ac.uk>
2022-07-04 17:09:58 +0100

    Added WIP CN summary statistic code

--- code/03-analysis.R
+++ code/03-analysis.R
@@ -13,3 +13,10 @@ barplot(table(analysis_data_na_removed$Condition))
 ## Age distribution
 summary(analysis_data_na_removed$age)
 plot(density(analysis_data_na_removed$age))
+
+## CN 1
+# density_cn <- apply(analysis_data_na_removed[, 5:21], 2, density)
+# 
+# plot(NA, xlim=range(sapply(density_cn, "[", "x")), ylim=range(sapply(density_cn, "[", "y")))
+# mapply(lines, density_cn, col=1:length(density_cn))
+# legend("topright", legend=names(density_cn), fill=1:length(density_cn))
