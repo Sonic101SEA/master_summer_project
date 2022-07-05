3bf94f1a88dae690533e045542707c2c0155b034
Shaun Tan <stan@35292brkdt.icr.ac.uk>
2022-07-05 10:54:10 +0100

    Added code to plot WGD data

--- code/03-exploratory_analysis.R
+++ code/03-exploratory_analysis.R
@@ -1,4 +1,9 @@
 
+# Libraries ---------------------------------------------------------------
+
+library(ggplot2)
+library(dplyr)
+
 # Data --------------------------------------------------------------------
 
 analysis_data <- read.csv(here::here("data/final_dataframe.csv"), row.names = 1)
@@ -6,17 +11,33 @@ analysis_data <- read.csv(here::here("data/final_dataframe.csv"), row.names = 1)
 ## Remove patients with missing data
 analysis_data_na_removed <- analysis_data[complete.cases(analysis_data), ]
 
+## Converting WGD to 1 and 0
+# analysis_data_na_removed$WGD <- as.numeric(analysis_data_na_removed$WGD)
+
 # Summary Statistics -----------------------------------------------------
 ## Distribution of sensitive and resistant patients
-barplot(table(analysis_data_na_removed$Condition))
+barplot(table(analysis_data_na_removed$Condition),
+        main = "Distribution of resistant and sensitive patients to platinum therapy",
+        ylab = "Frequency")
 
 ## Age distribution
 summary(analysis_data_na_removed$age)
+hist(analysis_data_na_removed$age, breaks = 10)
 plot(density(analysis_data_na_removed$age))
 
-## CN 1
+## CN
 # density_cn <- apply(analysis_data_na_removed[, 5:21], 2, density)
 # 
 # plot(NA, xlim=range(sapply(density_cn, "[", "x")), ylim=range(sapply(density_cn, "[", "y")))
 # mapply(lines, density_cn, col=1:length(density_cn))
 # legend("topright", legend=names(density_cn), fill=1:length(density_cn))
+
+## WGD
+analysis_data_na_removed %>%
+  count(Condition, WGD) %>%
+  ggplot(aes(x = Condition, y = n, fill = WGD)) + 
+  geom_bar(position = "dodge", stat = "identity") + 
+  geom_text(aes(label=n), position = position_dodge(width = 1), vjust = -1.0) +
+  theme_classic() + 
+  labs(title = "Distribution of WGD among resistant and sensitive groups", x = "Condition", y = "No. of patients", fill="WGD event")
+
