4191976ebd4b8df5a062767866837d0f04657d7b
Shaun Tan <shaun.tan21@imperial.ac.uk>
2022-07-11 15:07:05 +0100

    Added the reading of data for modelling

--- code/04-modelling_analysis.R
+++ code/04-modelling_analysis.R
@@ -1,4 +1,6 @@
 
 # Data --------------------------------------------------------------------
+analysis_data <- read.csv(here::here("data/final_dataframe.csv"), row.names = 1)
 
-
+## Remove patients with missing data
+analysis_data_na_removed <- analysis_data[complete.cases(analysis_data), ]
