7864eaf3c0a32d6904c3519b71ea46672a95d1c9
Shaun Tan <shaun.tan21@imperial.ac.uk>
2022-07-18 14:52:07 +0100

    Reorganised multivariate modelling code and added code for preparing data for unsupervised clustering

--- code/06-multivariate_modelling_analysis.R
+++ code/06-multivariate_modelling_analysis.R
@@ -2,7 +2,8 @@
 # Libraries ---------------------------------------------------------------
 
 library(glmnet)
-
+library(klaR)
+library(clustMixType)
 # Data --------------------------------------------------------------------
 
 modelling_data <- read.csv(here::here("data/final_dataframe.csv"), row.names = 1)
@@ -21,13 +22,40 @@ final_modelling_data$mhBRCA2 <- factor(final_modelling_data$mhBRCA2)
 final_modelling_data$Condition <- ifelse(final_modelling_data$Condition == "Resistant", 1, 0) # Resistant = 1, Sensitive = 0
 final_modelling_data$WGD <- ifelse(final_modelling_data$WGD == "TRUE", 1, 0) # TRUE = 1, FALSE = 0
 
-# Multivariate Analysis ---------------------------------------------------
-## Unsupervised clustering
-set.seed(1)
-columns_to_select <- c('ATM', 'NBN', 'CHEK1')
-labels <- subset(final_modelling_data, select = c('tumour_specimen_aliquot_id', 'Condition'))
-clustering_modelling_data <- subset(final_modelling_data, select =  columns_to_select)
-cluster_results <- kmodes(clustering_modelling_data, 2, 5)
+## Extracting labels
+labels <- subset(final_modelling_data, select = c('tumour_specimen_aliquot_id', 
+                                                  'Condition'))
 
-## Lasso
+## Setting row names then removing id columns
+rownames(final_modelling_data) <- final_modelling_data$tumour_specimen_aliquot_id
+final_modelling_data <- subset(final_modelling_data, 
+                               select = -c(TCGA_id, tumour_specimen_aliquot_id))
+
+
+# Multivariate Analysis - Unsupervised Clustering -------------------------
+set.seed(1)
+# Data Preparation
+
+
+## Selecting variables that have more than 0.25 p-value
+select_categorical <- c('ATM', 'NBN', 'CHEK1')
+
+select_mixed <- c('ATM', 'NBN', 'CHEK1', 'age', 'FAM175A', 'CX14',
+                  'PALB2', 'WGD', 'MRE11A', 'CX4', 'CX10', 'BRIP1',
+                  'CHEK2', 'CX3', 'RAD51C', 'BARD1', 'BRCA2')
+
+categorical_modelling_data <- subset(final_modelling_data, # Subset categorical data
+                                select =  select_categorical)
+
+mixed_modelling_data <- subset(final_modelling_data,
+                               select = select_mixed)
+
+# kmodes clustering - only for categorical variables
+
+kmodes_results <- kmodes(clustering_modelling_data, 2, 5)
+
+# K-prototype clustering
+
+
+# Lasso
 lasso_model <- cv.glmnet(x = data.matrix(final_modelling_data[4:ncol(final_modelling_data)]), y = final_modelling_data$Condition, family = "binomial", alpha = 1, nfolds = 10)
