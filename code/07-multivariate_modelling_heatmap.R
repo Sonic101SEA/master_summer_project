8ab464973c4db59d28282f3911da967aa4b71aeb
Shaun Tan <shaun.tan21@imperial.ac.uk>
2022-07-26 13:37:34 +0100

    Added code for heatmap of categorical variables

--- code/07-multivariate_modelling_heatmap.R
+++ code/07-multivariate_modelling_heatmap.R
@@ -22,17 +22,46 @@ heatmap_data_scaled_cn_melt <- melt(heatmap_data_scaled_cn)
 heatmap_data_scaled_cn_melt$X2 <- factor(heatmap_data_scaled_cn_melt$X2, levels = c('age', 'CX3', 'CX4', 'CX10', 'CX14'))
 
 ## For categorical data
-heatmap_data_scaled_categorical <- subset(heatmap_data_scaled, 
+heatmap_data_categorical_gene <- subset(heatmap_data_scaled, 
                                           select = -c(CX3, CX4, CX10, CX14,
                                                       kproto_clustering, kmedoids_clustering,
-                                                      hier_clustering, Condition,
+                                                      hier_clustering,
                                                       final_cluster_labels, age))
-heatmap_data_scaled_categorical_melt <- melt(heatmap_data_scaled_categorical)
+# heatmap_data_categorical_wgd <- subset(heatmap_data_scaled, 
+#                                                select = c(WGD))
+# 
+# heatmap_data_categorical_condi <- subset(heatmap_data_scaled, 
+#                                        select = c(Condition))
+
+
+heatmap_data_categorical_all <- melt(as.matrix(heatmap_data_categorical_gene))
+heatmap_data_categorical_all$value <- factor(heatmap_data_categorical_all$value, 
+                                             levels = c(' 0', ' 1', ' 2', '-1', '-2','TRUE', 'FALSE', 'Resistant', 'Sensitive'))
+heatmap_data_categorical_all$X2 <- factor(heatmap_data_categorical_all$X2,
+                                          levels = c('Condition', 'WGD', 'ATM', 'BARD1', 'BRCA2',
+                                                     'BRIP1', 'CHEK1', 'CHEK2', 'FAM175A',
+                                                     'MRE11A', 'NBN', 'PALB2', 'RAD51C'))
+
+
+# heatmap_data_scaled_categorical_melt_gene <- melt(as.matrix(heatmap_data_categorical_gene))
+# heatmap_wgd_melt <- melt(as.matrix(heatmap_data_categorical_wgd))
+# heatmap_condit_melt <- melt(as.matrix(heatmap_data_categorical_condi))
+
+# combined_categorical_melt <- merge(heatmap_data_scaled_categorical_melt_gene,
+#                                    heatmap_wgd_melt, by.x = 1, by.y = 1)
+# 
+# combined_categorical_melt2 <- merge(combined_categorical_melt,
+#                                     heatmap_condit_melt, by.x = 1, by.y = 1)
+
+
+# For clustering results
+
+
 
 # Plotting heatmap for continuous  -----------------------------------------
 heatmap_data_scaled_cn_melt %>%
   ggplot(aes(x = X1, y = X2, fill = value)) + 
-  geom_tile() + 
+  geom_tile() +
   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
 
 heatmap(heatmap_data_scaled_cn, scale = "none")
@@ -40,7 +69,11 @@ heatmap(heatmap_data_scaled_cn, scale = "none")
 
 # Plotting heatmap for categorical ----------------------------------------
 
-heatmap_data_scaled_categorical %>%
+heatmap_data_categorical_all %>%
   ggplot(aes(x = X1, y = X2, fill = value)) + 
-  geom_tile() + 
+  geom_tile(width = 0.9, height = 0.9) + 
   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+
+# Plotting heatmap for clustering results ---------------------------------
+
+
